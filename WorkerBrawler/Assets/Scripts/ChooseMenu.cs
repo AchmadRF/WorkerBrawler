030a963a6d628742da456256bbb66de107b1aa2f
rahmat <maulanarahmat352@gmail.com>
2019-01-23 11:55:51 +0700

    no message

--- WorkerBrawler/Assets/Scripts/ChooseMenu.cs
+++ WorkerBrawler/Assets/Scripts/ChooseMenu.cs
@@ -1,47 +1,64 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 
 public class ChooseMenu : MonoBehaviour {
 
-    public GameObject[] btn;
-    public int val,batasAtas,BatasBawah;
+    [SerializeField] tweener[] btn;
+
+    public int val = 0,batasAtas,BatasBawah;
     int plus;
     
     public bool change = false;
 
+
+    [System.Serializable]
+    public class tweener
+    {
+        public Transform obj;
+        public Vector3 posisiAkhir;
+        public Vector3 posisiAwal;
+        public float waktu = 0.2f;
+        public float delay = 0;
+        public Ease ease = Ease.OutBack;
+    }
     void Start()
     {
+        for (int i = 0; i < btn.Length; i++)
+        {
+            if (btn[i].ease == Ease.Unset)
+                btn[i].ease = Ease.OutBack;
+            btn[i].obj.DOMove(btn[i].posisiAwal, btn[i].waktu).SetDelay(btn[i].delay).SetEase(btn[i].ease);
+        }
+        change = true;
 
+        for (int i = 0; i < btn.Length; i++)
+        {
+            btn[i].obj.DOScale(0.95f, .9f).SetLoops(-1, LoopType.Yoyo);
+        }
+
+        
     }
 
     void Update()
     {
-        if (Input.GetAxis("Vertical") >= 0)
+        if (Input.GetAxis("Vertical") > 0)
         {
-            val += plus;
-            change = true;
-        }
-        if (Input.GetAxis("Vertical") <= 0)
-        {
+           
             val += -plus;
             change = true;
-        }if(Input.GetAxis("Vertical") == 0)
+        }
+        if (Input.GetAxis("Vertical") < 0)
         {
-            change = false;
+           
+           val += plus; 
+            change = true;
+
         }
 
-        if (change == true)
-        {
-            plus = 0;
-        }
-        else
-        {
-            plus = 1;
-        }
-
-        if(val > batasAtas)
+        if (val > batasAtas)
         {
             val = batasAtas;
         }
@@ -49,5 +66,43 @@ public class ChooseMenu : MonoBehaviour {
         {
             val = BatasBawah;
         }
+
+
+        if (Input.GetAxis("Vertical") == 0)
+        {
+            change = false;
+        }
+
+        if (change == true)
+        {
+            movingBtn();
+            plus = 0;
+        }
+        else
+        {
+            plus = 1;
+
+        }
+
+        
+       
+    }
+
+    void movingBtn()
+    {
+        
+        for(int i = 0;i<4; i++ )
+        {
+            if (i == val)
+            {
+                btn[i].obj.DOMove(btn[i].posisiAkhir, 0.2f);
+            }
+            else
+            {
+                btn[i].obj.DOMove(btn[i].posisiAwal, 0.2f);
+            }
+            
+        }
+
     }
 }
--- WorkerBrawler/Assets/Scripts/PlayerScr.cs
+++ WorkerBrawler/Assets/Scripts/PlayerScr.cs
@@ -1,19 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using DG.Tweening;
 public class PlayerScr : MonoBehaviour {
 
     public int player;
     public string attackBtn,dashBtn,jumpBtn;
-    
+
+    public LayerMask thisIsGround;
+    public Transform groundDetector;
     public Animator anim;
     SpriteRenderer thisSprite;
     Rigidbody2D PlayerRB;
+    float powerJump = 200,radius = 0.01f, powerDash = 200;
     public Transform[] colplace = new Transform[2];
     public Collider2D atkCol;
     string axis;
-    public bool walk = true;
+    public bool walk = true,onatk = true ,onJump = true , onDash = true;
+    public bool isGrounded,left, right;
+
 
 	// Use this for initialization
 	void Start () {
@@ -52,7 +57,7 @@ public class PlayerScr : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        if (Input.GetAxis(axis) != 0 && walk == true)
+        if (Input.GetAxis(axis) != 0 && walk == true && onJump == false)
         {
             anim.Play("Run");
         }
@@ -61,29 +66,56 @@ public class PlayerScr : MonoBehaviour {
         }
         if (Input.GetAxis(axis) > 0 && walk == true)
         {
+            right = true;
+            left = false;
             thisSprite.flipX = false;
             atkCol.transform.position = new Vector3(colplace[0].position.x, colplace[0].transform.position.y, 0);
             transform.Translate(new Vector3(4*Time.deltaTime, 0, 0));
         }
         if (Input.GetAxis(axis) < 0 && walk == true)
         {
+            left = true;
+            right = false;
             thisSprite.flipX = true;
             atkCol.transform.position = new Vector3(colplace[1].position.x,colplace[1].transform.position.y,0);
             transform.Translate(new Vector3(-4 * Time.deltaTime, 0, 0));
 
         }
-        if (Input.GetKeyDown(attackBtn))
+        if (Input.GetKeyDown(attackBtn) && onatk == true)
         {
             anim.Play("Attack");
         }
-        if (Input.GetKeyDown(dashBtn))
+        if (Input.GetKeyDown(dashBtn) && onDash == true)
         {
-            anim.Play("Dash");
+            dash();
+            
         }
-        if (Input.GetKeyDown(jumpBtn))
+        if (Input.GetKeyDown(jumpBtn) && onJump == false && isGrounded)
         {
+            isGrounded = false;
+            PlayerRB.AddForce(transform.up * powerJump);
             anim.Play("Jump");
         }
 
+    }
+
+    void dash()
+    {
+        anim.Play("Dash");
+
+        if (left == true)
+        {
+
+            PlayerRB.AddForce(Vector2.left*powerDash);
+        }
+        else
+        {
+            PlayerRB.AddForce(Vector2.right*powerDash);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundDetector.position, radius, thisIsGround);
     }
 }
